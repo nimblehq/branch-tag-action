6049b0356896d0133bbfaa402f0678f1181091b1
Hoang Mirs <hoang.mirs@gmail.com>
2020-11-12 15:40:24 +0700

    Update action to use the new environment files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ BRANCH_NAME="${REF#heads/}"
 TAG_NAME="${REF#tags/}"
 
 if [[ $BRANCH_NAME == master ]]; then
-  BRANCH_TAG=latest
-elif [[ $TAG_NAME != $REF ]]; then
-  BRANCH_TAG=$TAG_NAME
+    BRANCH_TAG=latest
+    elif [[ $TAG_NAME != $REF ]]; then
+    BRANCH_TAG=$TAG_NAME
 else
-  BRANCH_TAG="${BRANCH_NAME//\//-}"
+    BRANCH_TAG="${BRANCH_NAME//\//-}"
 fi
 
-echo "::set-env name=BRANCH_TAG::$BRANCH_TAG"
+echo "BRANCH_TAG=$BRANCH_TAG" >> $GITHUB_ENV
 echo "::set-output name=branch_tag::$BRANCH_TAG"
